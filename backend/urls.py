d7422228d7463e2191e0314abddbab756caae97c
nskMeDVeD <tarasov.a.s.1989@gmail.com>
2022-11-03 23:43:27 +0700

    Req.txt

--- backend/urls.py
+++ backend/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 from menu.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='hone'),
+    path('', index, name='home'),
     path('about/', contact, name='contact'),
     path('api/', include('api.urls')),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
